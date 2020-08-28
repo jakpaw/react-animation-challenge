c1dc2470f31e7067dc110df4963267d30ff0968c
Jakub Pawlot <jakub.pawlot@elpassion.pl>
2020-08-28 17:01:00 +0200

    Updates

---
+++ precache-manifest.bf26e33e3f54ec1430c16e5d91c26e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f553f7c3746e1877eb863eb500e507",
+    "url": "/react-animation-challenge/index.html"
+  },
+  {
+    "revision": "73170db27db0418a04b2",
+    "url": "/react-animation-challenge/static/css/main.2a9deca5.chunk.css"
+  },
+  {
+    "revision": "a14ac76b39af099e469d",
+    "url": "/react-animation-challenge/static/js/2.462a1544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-animation-challenge/static/js/2.462a1544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73170db27db0418a04b2",
+    "url": "/react-animation-challenge/static/js/main.b06ea914.chunk.js"
+  },
+  {
+    "revision": "be19070fec7c7f9825c0",
+    "url": "/react-animation-challenge/static/js/runtime-main.eca6b155.js"
+  },
+  {
+    "revision": "52e0119f32f057ea7c67a387caed747e",
+    "url": "/react-animation-challenge/static/media/handmaid.52e0119f.jpg"
+  }
+]);
